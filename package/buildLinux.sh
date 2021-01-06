775c1844986d31a3d9e46c67f0cc8b80e6f26d4f
ppenguin <hieronymusv@gmail.com>
2021-01-06 16:04:20 +0100

    [WIP] First attempt to fix build on (Manjaro) Linux with more flexible VST2 switch

--- package/buildLinux.sh
+++ package/buildLinux.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+set -e
+
+USE_VST=${USE_VST:-0}
+NPROC=$(nproc)
+NPROC=${NPROC:-3}
+
 rm -rf build-linux-x86_64
-cmake -B build-linux-x86_64 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DFFMPEG_ROOT=/mnt/audio/ag-deps-linux-x86_64
-cmake --build build-linux-x86_64 -j3
+cmake -B build-linux-x86_64 -DUSE_VST=${USE_VST} -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build build-linux-x86_64 -j${NPROC}
 
 VERSION=$(cat package/VERSION)
 
@@ -16,9 +22,9 @@ cp -r build-linux-x86_64/Plugin/AudioGridderFx_artefacts/RelWithDebInfo/VST3/Aud
 cp -r build-linux-x86_64/Plugin/AudioGridderInst_artefacts/RelWithDebInfo/VST3/AudioGridderInst.vst3 package/build/vst3/
 cp -r build-linux-x86_64/Plugin/AudioGridderMidi_artefacts/RelWithDebInfo/VST3/AudioGridderMidi.vst3 package/build/vst3/
 
-cp package/build/vst/* ../Archive/Builds/$VERSION/linux
+cp package/build/vst/* ../Archive/Builds/$VERSION/linux || echo "No VST plugins built."
 cp -r package/build/vst3/* ../Archive/Builds/$VERSION/linux
 
 cd package/build
-zip -r AudioGridder_$VERSION-Linux.zip vst vst3
+tar zcvf AudioGridder_$VERSION-Linux.tgz vst vst3
 rm -rf vst vst3
